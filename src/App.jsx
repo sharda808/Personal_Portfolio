c7d36f113f024cf67b3da1f4f4cbb4673d903208
sharda <shardakumari96115@gmail.com>
2026-01-08 13:58:46 +0530

    This is portfoli

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero"
+
+
+function App() {
+
+
+  return (
+    <>
+ <h1 className="text-3xl font-bold underline">This is My portfilo</h1>
+ <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = 
+() => {
+return (
+  <section className="min-h-screen flex items-center justify-center px-6">
+    <div className="max-w-4xl text-center bg-white p-10 rounded-3xl shadow-xl">
+<h1 className="text-4xl md:text-5xl font-extrabold mb-4">
+  Hi, I'm <span className="text-blue-600">sharda jha</span>
+</h1>
+<p>Full stack Develpoer | React • Node.js • MongoDB
+<br/>
+Building smart, real-world web application 
+</p>
+</div>
+</section>
+) 
+}
+export default Hero;
